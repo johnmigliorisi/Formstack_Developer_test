fd75d02a34c541c8ed41a65b147aaade6623c33e
John Migliorisi <johnmigliorisi@gmail.com>
2016-10-29 13:27:56 -0400

    sql schema

---
+++ sql/users.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE IF NOT EXISTS `formstack_exercise`;
+
+USE `formstack_exercise`;
+
+CREATE TABLE IF NOT EXISTS `user` (
+`user_id` int unsigned NOT NULL AUTO_INCREMENT,
+`email` VARCHAR(255) NOT NULL,
+`first_name` VARCHAR(255) NOT NULL,
+`last_name` VARCHAR(255) NOT NULL,
+`password` VARCHAR(255) NOT NULL,
+PRIMARY KEY (`user_id`),
+UNIQUE INDEX `email_unique_index` (`email`)
+) ENGINE = InnoDB;
