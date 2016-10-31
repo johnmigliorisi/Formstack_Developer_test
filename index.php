57426c6481f660378bfc7147b5f73977b5e42bfd
John Migliorisi <johnmigliorisi@gmail.com>
2016-10-30 20:38:25 -0400

    added delete button and processing

--- index.php
+++ index.php
@@ -3,6 +3,11 @@ require_once ('controller/UserController.php');
 
 $userObj = new UserController();
 $users = $userObj->find();
+if (!empty($_GET['id'])) {
+    // Update and redirect to home
+    $userObj->delete($_GET['id']);
+    header('Location: index.php');
+} 
 ?>
 <html>
     <head>
@@ -12,7 +17,7 @@ $users = $userObj->find();
         <?php if ($users): ?>
             <ul>
                 <?php foreach ($users as $user): ?>
-                <li><span><?= $user['firstName'] ?> <?= $user['lastName'] ?></span> <a href="edit.php?id=<?= $user['id'] ?>">Edit</a></li>
+                <li><span><?= $user['firstName'] ?> <?= $user['lastName'] ?></span> <a href="edit.php?id=<?= $user['id'] ?>">Edit</a> <a href="index.php?id=<?= $user['id'] ?>">Delete</a></li>
                 <?php endforeach; ?>
             </ul>
         <?php else: ?>
